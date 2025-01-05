2c720212714430d9470afde2baa607a48a2768c6
Gregorio Garofalo <garofalogregorio@gmail.com>
2025-01-05 11:26:18 +0100

    Eliminato le informazioni sensibili dal con pool

--- src/main/java/model/ConPool.java
+++ src/main/java/model/ConPool.java
@@ -44,10 +44,7 @@ public class ConPool {
 	public static Connection getConnection() throws SQLException {
 		if (datasource == null) {
 			PoolProperties p = new PoolProperties();
-			p.setUrl("jdbc:mysql://mysqlservice-garofalogregorio-8ce9.b.aivencloud.com:12556/aemme?serverTimezone=" + TimeZone.getDefault().getID());
-			p.setDriverClassName("com.mysql.cj.jdbc.Driver");
-			p.setUsername("avnadmin");
-			p.setPassword("AVNS_GafeXJM0VL_EhVTfLfC");
+
 			p.setMaxActive(100);
 			p.setInitialSize(10);
 			p.setMinIdle(10);
